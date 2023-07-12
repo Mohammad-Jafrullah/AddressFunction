bfd7a2ac2a03533f67c2c9ebd84ae3aead3e89fc
Jafrullah <fardeenalam078@gmail.com>
2023-07-12 17:07:30 +0530

    Updates

---
+++ precache-manifest.baf911763e2ac943d2738e96e75f166a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c31f521ed6990ed8dd4d",
+    "url": "/AddressFunction/static/css/main.7e66bcac.chunk.css"
+  },
+  {
+    "revision": "c31f521ed6990ed8dd4d",
+    "url": "/AddressFunction/static/js/main.43ba2fd7.chunk.js"
+  },
+  {
+    "revision": "4b2ebcc337d5d47f402e",
+    "url": "/AddressFunction/static/js/runtime~main.0fa636bd.js"
+  },
+  {
+    "revision": "4f5ede536014727a4d5f",
+    "url": "/AddressFunction/static/js/2.5233fa45.chunk.js"
+  },
+  {
+    "revision": "1d7a7846e69ca36444bd",
+    "url": "/AddressFunction/static/js/3.1e9de0c8.chunk.js"
+  },
+  {
+    "revision": "0ce4c01af7e3424d1370cf9acefa6a84",
+    "url": "/AddressFunction/index.html"
+  }
+];
